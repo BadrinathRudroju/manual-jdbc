22f44681ca64a56f73b9cfde40a432b8f5e43d6c
Badrinath Rudroju <rudrojubadrinath27@gmail.com>
2026-06-30 10:09:21 +0530

    add crud operations on sql

--- src/main/java/com/badrinath/JDBCdemo.java
+++ src/main/java/com/badrinath/JDBCdemo.java
@@ -1,15 +1,48 @@
 package com.badrinath;
 
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.PreparedStatement;
-import java.sql.SQLException;
+import java.sql.*;
 
 public class JDBCdemo {
     public static void main(String[] args){
 
-        try(Connection connection = DriverManager.getConnection("jdbc:h2:mem:;INIT=RUNSCRIPT FROM 'classpath:users.sql';")){
+        try(Connection connection = DriverManager
+                .getConnection("jdbc:h2:mem:;INIT=RUNSCRIPT FROM 'classpath:users.sql';")){
             System.out.println("connection.isValid(0) =" + connection.isValid(0));
+
+            //select
+            PreparedStatement ps = connection
+                    .prepareStatement("SELECT * FROM users where name = ?");
+            ps.setString(1,"Badri");
+            ResultSet result = ps.executeQuery();
+            while(result.next()){
+                System.out.println(result.getInt("id") + "-" + result.getString("name"));
+            }
+
+
+            //insert
+            PreparedStatement insertPs = connection
+                    .prepareStatement("INSERT INTO users (name) values (?)");
+            insertPs.setString(1,"bablu");
+            int insertCount = insertPs.executeUpdate();
+            System.out.println("insertCount: " + insertCount);
+
+
+            //update
+            PreparedStatement updatePs = connection
+                    .prepareStatement("update users set name = ? where name = ?");
+            updatePs.setString(1,"badri");
+            updatePs.setString(2,"bablu");
+            int updateCount = updatePs.getUpdateCount();
+            System.out.println("updateCount: " + updateCount);
+
+
+            //delete
+            PreparedStatement deletePs = connection
+                    .prepareStatement("delete from users where name = ?");
+            deletePs.setString(1,"badri");
+            int deleteCount = deletePs.getUpdateCount();
+            System.out.println("deleteCount: " + deleteCount);
+
         }catch(SQLException e){
             e.printStackTrace();
         }
